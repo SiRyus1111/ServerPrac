e99721464c98967f7efb9356dbedf50ea50421f3
SiRyus1111 <anniefun1111@gmail.com>
2026-02-16 03:17:27 +0900

    26.2.16 과제 내역 추가..

---
+++ src/ServerPrac/TCP IP Socket 3-4/TCP IP Socket 3-4.cpp
@@ -0,0 +1,141 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <winsock2.h>
+#include "Common.h"
+#include <ws2tcpip.h>
+
+
+#define TESTNAME "www.google.com"
+
+/*
+	오늘은 DNS와 이름 변환 함수
+
+	도메인 네임은 사람이 읽을 수 있는 형태의 주소 체계이다. (예 : www.google.com)
+	물론 그냥 IP 주소로 접속해도 되지만, 사람이 IP 주소를 외우기 어렵기 때문에 도메인 네임이 사용된다.
+
+	근데 네트워크 통신을 하려면 당연히 사용자가 입력한 도메인 네임을 숫자(바이너리) 형태의 IP 주소로 변환해야한다..
+
+	도메인 네임과 IP 주소의 변환 정보는 인터넷에 존재하는 여러 DNS(Domain Name System) 서버가 관리한다.
+	한 DNS 서버가 모든 도메인 네임과 IP 주소의 변환 정보를 관리하는건 아니다.
+	어떻게 보면 분산 DB?
+
+	DNS는 나중에 네트워크 더 공부할 때 살펴보자..
+
+	일단 이 책에서는 DNS가 정상적으로 작동한다고 가정하고 진행한다고 한다.
+
+	뭐,일단.
+	응용 프로그램이 도메인 네임 <-> IP 주소 간 변환을 할 수 있도록 다음과 같은 소켓 함수가 제공된다.
+
+	gethostbyname() : 도메인 네임 -> IPv4 주소 변환 함수
+	gethostbyname(const char *도메인_네임);
+
+	gethostbyaddr() : IPv4 주소 -> 도메인 네임 변환 함수
+	gethostbyaddr(const char *바이너리_IP_주소_포인터, int 주소_길이,int 주소_체계);
+
+	이 두 함수는 구식이라 요즘은 getaddrinfo()/getnameinfo() 함수를 사용한다.
+	일단 이번에는 책에 나와있는 대로 구식 함수인 gethostbyname()/gethostbyaddr() 함수를 사용해서 실습을 해봤다.'
+
+	getaddrinfo()/getnameinfo()는 좀 이따 알아보자.
+
+	그리고 이 두 구식 함수는 모두 hostent 구조체를 반환한다.
+
+	struct hostent {
+		char *공식_도메인_네임;
+		char **별명_목록(aliase name)(참조 변수 느낌. 한 호스트의 도메인 네임이 여러개일 때.);
+		short 주소_체계(AF_INET/AF_INET6);
+		short 주소_길이(바이트 단위, 4(IPv4)/16(IPv6));
+		char **주소_목록(네트워크 바이트 정렬. 한 호스트가 IP 주소를 여러개 가지고 있으면 이 포인터를 따라가면 모든 ip 주소를 얻을 수 있다.);
+	}
+
+	뭐, 이 정도면 될 듯.
+*/
+
+// 도메인 네임을 입력받아 해당 도메인의 IPv4 주소를 얻는 함수
+// gethostbyname() 함수 사용
+bool GetIPAddr(const char* name, struct in_addr* addr) { // name : 도메인 네임, addr : IPv4 주소를 저장할 in_addr 구조체 포인터
+
+	struct hostent* addr_ptr = gethostbyname(name); // 도메인 네임 -> hostent 구조체 포인터
+	if (addr_ptr == NULL) { // 실패하면(ptr 값이 NULL)
+		err_display("gethostbyname()"); // 오류 출력
+		return false; // false 반환
+	}
+	if (addr_ptr->h_addrtype != AF_INET || addr_ptr->h_addr_list[0] == NULL) { // 주소 체계가 IPv4가 아니거나 주소 목록이 비어있으면
+		return false; // false 반환
+	}
+	memcpy(addr,addr_ptr->h_addr_list[0], addr_ptr->h_length); // addr 포인터가 가리키는 in_addr 구조체에 IPv4 주소 복사
+	return true; // true 반환
+}
+
+// IPv4 주소를 입력받아 해당 주소의 도메인 네임을 얻는 함수
+// gethostbyaddr() 함수 사용
+bool GetDomainName(struct in_addr addr, char* name, int namelen) { // addr : IPv4 주소, name : 도메인 네임을 저장할 문자열 버퍼, namelen : 문자열 버퍼의 크기
+
+	struct hostent* dns_ptr = gethostbyaddr((const char*)&addr, sizeof(addr), AF_INET); //  IPv4 주소 -> hostent 구조체 포인터
+	if (dns_ptr == NULL) { // 실패하면(ptr 값이 NULL)
+		err_display("gethostbyaddr()"); // 오류 출력
+		return false; // false 반환
+	}
+	if (dns_ptr->h_addrtype != AF_INET) { // 주소 체계가 IPv4가 아니면
+		return false; // false 반환
+	}
+	strncpy(name, dns_ptr->h_name, namelen); // name 버퍼에 도메인 네임 복사
+	return true; // true 반환
+}
+
+/*
+	추가로, 유효한 도메인 네임을 IP 주소로 변환하는 일은 가능하지만서도,
+	유효한 IP 주소를 도메인 네임으로 변환하는건 불가능할 수도 있다.
+		
+	왜냐면, 도메인 네임과 IP 주소의 변환 정보는 DNS 서버에 저장되는데, 
+	모든 IP 주소에 대해 도메인 네임이 존재하는건 아니기 때문이다.
+	(특히 개인용 IP 주소라면 더더욱.)
+
+	마지막으로, hostent 구조체는 스레드당 하나씩 할당된다.
+	그러니까, gethostbyname()/gethostbyaddr() 함수가 반환하는 hostent 구조체는
+	내부 정적/공유 버퍼를 가리킬 수 있어서
+	다음 호출로 내용이 덮어써질 수 있다.
+	그래서 변환된 IP 주소나 도메인 네임을 계속 사용하려면 
+	다른 소켓 함수를 호출하기 전에 꼭 복사해 놓아야 한다.
+
+	그래서 GetIPAddr()/GetDomainName() 함수에서는
+	복사본을 반환하도록 했다..
+
+	getaddrinfo()/getnameinfo() 함수도 조금만 알아보자.
+
+	getaddrinfo() 함수 : 도메인 네임 -> IP 주소 변환 함수
+	getaddrinfo(const char *도메인_네임, const char *서비스_이름, const struct addrinfo *힌트, struct addrinfo **결과_리스트);
+
+	getnameinfo() 함수 : IP 주소 -> 도메인 네임 변환 함수
+	getnameinfo(const struct sockaddr *소켓_주소_구조체_포인터, int 소켓_주소_구조체_크기, char *호스트_버퍼, int 호스트_버퍼_크기, char *서비스_버퍼, int 서비스_버퍼_크기, int 플래그);
+
+	이건 나중에 직접 쓰면서 알아보자.. 지금은 체력이 딸린다..
+*/
+
+int main(int argc, char *argv[]) {
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		std::cout << "[Error] 윈속 초기화 실패\n";
+		return 1;
+	}
+
+	std::cout << "[Notice] 윈속 초기화 성공\n";
+
+	std::cout << "도메인 네임 : " << TESTNAME << '\n';
+	struct in_addr addr;
+	if (GetIPAddr(TESTNAME, &addr)) {
+		char str[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &addr, str, sizeof(str));
+		std::cout << "IPv4 주소 : " << str << '\n';
+
+		char domainName[256];
+		if (GetDomainName(addr, domainName, sizeof(domainName))) {
+			std::cout << "도메인 네임(다시 변환 후) : " << domainName << '\n';
+		}
+	}
+
+	WSACleanup();
+	return 0;
+}
---
+++ src/ServerPrac/TCP IP Socket 3-ex1/TCP IP Socket 3-ex1.cpp
@@ -0,0 +1,176 @@
+﻿#include <iostream> // 표준 입출력(std::cin, std::cout)
+#include <winsock2.h> // 윈속2 API
+#include <ws2tcpip.h> // inet_pton(), getaddrinfo() 등 소켓 함수와 구조체
+#include <string.h> // 문자열 라이브러리 3대장
+#include <string>
+#include <cstring>
+
+
+#pragma comment(lib, "ws2_32.lib")
+
+// ipparse 함수 구현
+// ip 주소를 받아, 주소 체계를 출력하고 다시 문자렬로 바꿔 출력하는 함수
+// IPv4도 IPv6도 아닌 ip 주소라면 invaild_ip를 출력한다.
+int cmd_ipparse(const std::string& ip) { // ip 주소를 문자열 형식으로 받는다
+
+	in_addr ipv4_addr{}; // IPv4 바이너리 주소를 담을 구조체
+
+	if (inet_pton(AF_INET, ip.c_str(), &ipv4_addr) == 1) { // 주소 체계에 맞지 않는 ip 주소가 입력된다면 inet_pton() 함수는 0을 반환한다. 물론 성공하면 1을 반환한다.
+
+		char ipv4_str[INET_ADDRSTRLEN]{}; // INET_ADDRSTRLEN으로 IPv4 주소가 들어갈 최대 버퍼 크기만큼의 ipv4_str 문자열을 만든다.
+		if (inet_ntop(AF_INET, &ipv4_addr, ipv4_str, sizeof(ipv4_str)) == nullptr) { // IPv4 바이너리 주소를 담은 in_addr 구조체의 주소를 다시 문자열로 바꾼다. 물론 실패 예외처리도 함.
+			std::cout << "invalid_ip\n";
+			return 1;
+		}
+		
+		std::cout << "family=IPv4\n" << "roundtrip=" << ipv4_str << '\n'; // 출력
+		return 0; // 함수 종료
+	}
+
+	in6_addr ipv6_addr; // IPv6 바이너리 주소를 담을 구조체
+
+	if (inet_pton(AF_INET6, ip.c_str(), &ipv6_addr) == 1) { // 주소 체계에 맞지 않는 ip 주소가 입력된다면 inet_pton() 함수는 0을 반환하고, 성공하면 1을 반환하기 때문에, IPv6 주소임을 판별하기 위해 inet_pton() == 0을 조건으로 넣는다.
+
+		char ipv6_str[INET6_ADDRSTRLEN]{}; // INET6_ADDRSTRLEN으로 IPv6 주소가 들어갈 최대 버퍼 크기만큼의 ipv4_str 문자열을 만든다.
+		if (inet_ntop(AF_INET6, &ipv6_addr, ipv6_str, sizeof(ipv6_str)) == nullptr) { // IPv6 바이너리 주소를 담은 in6_addr 구조체의 주소를 다시 문자열로 바꾼다. 물론 실패 예외처리도 함.
+			std::cout << "invalid_ip\n";
+			return 1;
+		}
+
+		std::cout << "family=IPv6\n" << "roundtrip=" << ipv6_str << '\n'; // 출력
+		return 0; // 함수 종료
+	}
+
+	std::cout << "invalid_ip\n"; // IPv4와 IPv6 주소 둘 다 아니라면 invaild_ip를 출력한다.
+
+	return 1; // 함수 종료
+}
+
+// endian 함수 구현
+// 포트 번호를 받아 출력 후, 네트워크 바이트 정렬(16진수)로 한 번, 다시 원래대로 호스트 바이트 정렬로 한 번 출력하는 함수.
+int cmd_endian(const u_short& port_num) { // u_short(0~65535) 형식으로 포트 번호를 받는다.
+
+	std::cout << "host=" << port_num << '\n'; // 입력받은 포트 번호 출력
+
+	u_short net_port_num = htons(port_num); // 포트 번호를 htons() 함수로 네트워크 바이트 정렬로 변환
+	std::cout << std::hex << "net=0x" << net_port_num << '\n'; // 16진수로 출력
+
+	u_short host_port_num = ntohs(net_port_num); // 포트 번호를 ntohs() 함수로 다시 호스트 바이트 정렬로 변환
+	std::cout << std::dec << "back=" << host_port_num << '\n'; // 10진수로 출력
+
+	return 0; // 함수 종료
+}
+
+
+// resolve 함수 구현
+// 호스트(도메인 네임/IP 주소) 정보와 서비스(포트 번호/서비스 명) 정보를 받아서 IPv4면 IPv4 주소로, IPv6면 IPv6 주소로 출력하는 함수.
+int cmd_resolve(const std::string& host, const std::string& service) { // host와 service를 참조 변수로 받는다. 
+
+	addrinfo hints{}; // addrinfo 구조체 형식의 hints 구조체의 모든 멤버를 0으로 초기화
+	hints.ai_family = AF_UNSPEC; // hints 구조체의 주소 체계 필드에 IPv4/IPv6 둘 다 해당하도록 AF_UNSPEC을 대입한다.
+	hints.ai_socktype = SOCK_STREAM; // hints 구조체의 소켓 타입 필드에 신뢰성 있는/연결형 통신을 의미하는 SOCK_STREAM을 대입한다. 
+	hints.ai_protocol = IPPROTO_TCP; // hints 구조체의 프로토콜 필드에 TCP를 의미하는 IPPROTO_TCP를 대입한다.
+
+	addrinfo* result = nullptr; // addrinfo 구조체 형식의 result 포인터 변수를 nullptr로 초기화한다.
+
+	int rc = getaddrinfo(host.c_str(), service.c_str(), &hints, &result); // getaddrinfo() 함수를 호출하고, 예외 처리를 하기 위해 리턴값을 변수 rc에 저장한다.
+
+	if (rc != 0) { // getaddrinfo() 함수는 성공시 0을 반환하고, 실패시 오류 코드를 반환하므로, rc != 0이라는 조건으로 실패를 판별한다.
+		std::cout << "resolve_failed " << rc << '\n';
+
+		return 1; // 함수 종료
+	}
+
+	// result 노드부터 쭉 연결 리스트를 따라가도록 for 문을 돌린다.
+	for (addrinfo* p = result; p != nullptr; p = p->ai_next) { // for (addrinfo 구조체 포인터 p = result 구조체 포인터 result부터 시작; 만약 p == nullptr(다음 노드가 없으면)이면 종료; p = 현재 p의 다음 노드의 주소)
+
+		char ipstr[INET6_ADDRSTRLEN]{}; // ip주소를 저장할 변수(IPv6의 최대 버퍼 크기로 지정)(전부 0으로 초기화)
+
+		if (p->ai_family == AF_INET) { // 주소 체계가 IPv4면
+			auto* ipv4 = (sockaddr_in*)p->ai_addr; // IPv4 소켓 주소 구조체인 sockaddr_in에 포인터로 할당
+			if (inet_ntop(AF_INET, &(ipv4->sin_addr), ipstr, sizeof(ipstr)) != nullptr) { // 소켓 주소 구조체의 IP 주소를 문자열로 변환, 실패 예외처리
+				std::cout << "IPv4 " << ipstr << '\n'; // IP 주소를 문자열로 출력
+			}
+		}
+
+		else if (p->ai_family == AF_INET6) { // 주소 체계가 IPv6면
+			auto* ipv6 = (sockaddr_in6*)p->ai_addr; // IPv6 소켓 주소 구조체인 sockaddr_in6에 포인터로 할당
+			if (inet_ntop(AF_INET6, &(ipv6->sin6_addr), ipstr, sizeof(ipstr)) != nullptr) { // 소켓 주소 구조체의 IP 주소를 문자열로 변환, 실패 예외처리
+				std::cout << "IPv6 " << ipstr << '\n'; // IP 주소를 문자열로 출력
+			}
+		}
+	}
+
+	freeaddrinfo(result); // freeaddrinfo() 함수로 자원 반환
+
+	return 0; // 함수 종료
+}
+
+int main() {
+
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		std::cout << "[Error] 윈속 초기화 실패";
+		return 1;
+	}
+
+	// 명령어를 받을 문자열
+	std::string command;
+
+	// 첫 번째 명령어 입력받기
+	std::cin >> command;
+
+	if (command != "NetTool") { // 입력받은 명령어의 첫 번째 문자열이 NetTool이 아니면
+		std::cout << "사용법 :\n" // 사용법 출력
+			<< "NetTool ipparse <ip> : IP 문자열 <-> 바이너리 변환\n"
+			<< "NetTool endian <uint16_port> : 바이트 오더 변환\n"
+			<< "NetTool resolve <host> <service> : 도메인 네임 해석 결과 출력\n";
+		WSACleanup();
+		return 1; // main() 함수 종료
+	}
+	
+	// 두 번째 명령어 입력받기
+	std::cin >> command;
+	if (command == "ipparse") { // 입력받은 문자열이 ipparse면
+		std::string ip; // string 형으로 ip 주소 저장할 변수 만들기
+		
+		if (!(std::cin >> ip)) { // std::cin 실패 예외처리
+			std::cout << "사용법 : ipparse <ip>\n";
+			WSACleanup();
+			return 1;
+		}
+
+		cmd_ipparse(ip); // ipparse() 함수 호출해서 주소 체계와 IP 주소 출력
+	}
+	else if (command == "endian") { // 입력받은 문자열이 endian이면
+		u_short port; // 포트 번호의 범위에 해당한 u_short(unsigned short) 형으로 포트 번호 저장할 변수 만들기
+
+		if (!(std::cin >> port)) { // std::cin 실패 예외처리
+			std::cout << "사용법 : endian <port>\n"; 
+			WSACleanup();
+			return 1;
+		}
+
+		cmd_endian(port); // endian() 함수 호출해서 포트 번호 출력
+	}
+
+	else if (command == "resolve") { // 입력받은 문자열이 resolve면
+		std::string host, service; // sstring 형으로 host(IP 주소/도메인 네임), service(포트 번호/서비스 명)을 저장할 변수 만들기
+
+		if (!(std::cin >> host >> service)) { // std::cin 실패 예외처리
+			std::cout << "사용법 : resolve <host> <service>\n";
+			WSACleanup();
+			return 1;
+		}
+
+		cmd_resolve(host, service); // resolve() 함수 호출해서 주소 체계와 IP 주소 출력
+	}
+
+	WSACleanup(); // 윈속 종료(자원 반환)
+
+	return 0; // main() 함수(프로그램) 종료
+}
